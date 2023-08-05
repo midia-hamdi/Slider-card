25bd0d722de7661527a97962d0a081ee6967e8bd
midia-hamdi <midiahamdi367@gmail.com>
2023-08-05 16:42:28 +0330

    add first slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Arrow from './Arrow'
+import Card from './Card'
+
+export default function Slider() {
+  return (
+    
+        <div className="slide-container">
+
+            <div className="wrapper">
+                    <Arrow />
+                    <Card />
+                    <Arrow />
+
+            </div>
+        </div>
+            )
+            }
